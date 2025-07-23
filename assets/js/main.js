2a1472660e26544f714add2369733a3ce3a963c9
Yann <yannguimaraes5@outlook.com>
2025-07-23 02:08:20 -0300

    adding requests to pokeApi and some CSS changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,29 @@
-// Não tem ainda :(
-const raid = 'gay';
-const raid = 'gado';
 
-console.log(raid);
+function convertPokemonToList (pokemon){
+    return `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+                
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}<\li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}" 
+                alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById("pokemonList")
+                  
+
+pokeApi.getPokemons().then((pokemons = []) => {
+
+        pokemonList.innerHTML += pokemons.map(convertPokemonToList).join('')  
+
+    })
+    .catch((error) => console.log(error))
+    
