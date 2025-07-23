42e54f685a5f6f37cb62dd4fdae0609ae6f4e2bb
Yann <yannguimaraes5@outlook.com>
2025-07-23 03:12:32 -0300

    adding pagination button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,50 @@
 
-function convertPokemonToList (pokemon){
-    return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
-                
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}<\li>`).join('')}
-                </ol>
+const pokemonList = document.getElementById("pokemonList")
+const loadMoreButton = document.getElementById("loadMoreButton")
+const maxRecords = 151
+const limit = 10
+let offset = 0
+                  
+function loadPokemonItens(offset, limit){
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => 
+            `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                    
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}<\li>`).join('')}
+                    </ol>
+    
+                    <img src="${pokemon.photo}" 
+                    alt="${pokemon.name}">
+                </div>
+            </li>
+        `
+        ).join('')  
 
-                <img src="${pokemon.photo}" 
-                alt="${pokemon.name}">
-            </div>
-        </li>
-    `
+        pokemonList.innerHTML += newHtml
+    })
+.catch((error) => console.log(error))
 }
 
-const pokemonList = document.getElementById("pokemonList")
-                  
+loadPokemonItens(offset, limit)
 
-pokeApi.getPokemons().then((pokemons = []) => {
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
 
-        pokemonList.innerHTML += pokemons.map(convertPokemonToList).join('')  
+    const qtdRecordWithNextPage = offset + limit
 
-    })
-    .catch((error) => console.log(error))
+    if(qtdRecordWithNextPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+
     
+})
